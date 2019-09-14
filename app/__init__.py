7fb0b5b83d6d00fd6a0fa2177c432fc7ba0a6b0d
washucode <estherw8525@gmail.com>
2019-09-14 21:10:52 +0300

    initialize flask login extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def  pitch_app():
 
     app.config.from_object(Config)
+
+    # initialize extensions
+    login_manager.init_app(app)
 
     # registering main blueprint
     from .main import main as main_blueprint
