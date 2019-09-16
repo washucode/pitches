b09b2a7642437e8c7432350d58cc07bcfc38ca21
washucode <estherw8525@gmail.com>
2019-09-16 03:07:25 +0300

    add Pitches and Comment models

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,33 +2,35 @@ from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
 from .forms import LoginForm,SignUpForm
+from ..models import User
 
 @auth.route('/signup',methods=['GET','POST'])
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
         user =  User(email = form.email.data, username = form.username.data,password = form.password.data)
-        user.set_password(password)
-        user.save()
+        
+        user.save_user()
 
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/sign_up.html',signup_form = form)
+    return render_template('sign_up.html',signup_form = form)
 
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
-        if user is not None and user.check_password(form.password.data):
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is not None and user.verify_password(form.password.data):
             login_user(user,form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
-        flash('Invalid username or Password')
+        else:
+            flash('Invalid username or Password')
 
 
-    return render_template('auth/login.html',login_form = form)
+    return render_template('login.html',login_form = form)
 
 @auth.route('/logout')
 @login_required
--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
 class User(UserMixin,db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key= True)
     username = db.Column(db.String(255),nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable =False,unique=True)
-    password = db.Column(db.String(255),nullable = False)
+    email = db.Column(db.String(20), nullable =False,unique=True)
+    bio = db.Column(db.String(255))
+    profile_img = db.Column(db.String(20),nullable =False,default='default.jpg')
+    password = db.Column(db.String(60),nullable = False)
+    pitches = db.relationship('Pitch', backref = 'user', lazy = 'dynamic')
+    comments = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
 
     def save_user(self):
         db.session.add(self)
@@ -15,20 +20,58 @@ class User(UserMixin,db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def set_password(self,password):
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password atrribute')
+    
+    @password.setter
+    def password(self, password):
         password_hash = generate_password_hash(password)
         self.password = password_hash
-        
-    def check_password(self, password):
-        return check_password_hash(self.password,password)
-    
 
-    
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
     def __repr__(self):
-        return "User:%s"%str(self.username)
+        return f'User {self.username}'
+
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(20))
+    pitch_content = db.Column(db.String)
+    category = db.Column(db.String(20))
+    author = db.Column(db.String(20))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)        
+    date_posted = db.Column(db.DateTime, default = datetime.utcnow)    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'pitch', lazy = 'dynamic')
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)    
+    body = db.Column(db.String)          
+    date_posted = db.Column(db.DateTime, default = datetime.utcnow)    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+        
         
 @login_manager.user_loader
-def user_loader(user_id):
-    return User.query.get(user_id)
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
     
