8af54a40763df3b3d6e6275b60f5d35e1bc46759
washucode <estherw8525@gmail.com>
2019-09-18 04:14:26 +0300

    solvedeployment errors

--- config.py
+++ config.py
@@ -4,3 +4,16 @@ class Config:
      SECRET_KEY = '435313ea80b5a872114356a1'
      SQLALCHEMY_DATABASE_URI = os.environ.get['DATABASE_URL']
      UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
