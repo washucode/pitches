7eee421d2ba6f0c1558cc9ee6c2f61e238848620
washucode <estherw8525@gmail.com>
2019-09-18 16:15:32 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY=435313ea80b5a872114356a1
 export APP_SETTINGS="config.DevelopmentConfig"
 export DATABASE_URL='postgresql+psycopg2://esther:p@localhost/pitches'
-python3 manage.py db upgrade
+python3 manage.py shell
